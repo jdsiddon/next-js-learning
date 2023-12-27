69d1b796b1bdc5ee5d928654aa78afc405c40896
jsiddon <justinsiddon@gmail.com>
2023-12-27 14:54:12 -0800

    Updated.

---
+++ app/lib/actions.ts
@@ -0,0 +1,121 @@
+"use server";
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// Validate schema
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.'
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' })
+  ,
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
+  date: z.string(),
+})
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+}
+
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Note using "safeParse"
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  });
+  
+  // Form validation failed
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.'
+    }
+  }
+
+  // Valid fields to all to continue on
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice'
+    };
+  }
+
+  // Clear the cache on the users browser and fetch new data.
+  revalidatePath('/dashboard/invoices');
+
+  // Redirect back to their path
+  redirect('/dashboard/invoices')
+}
+
+// Logic to update invoices
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice'
+    };
+  }
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+export async function deleteInvoice(id: string, formData: FormData) {
+  throw new Error("Failed to delete invoice!");
+
+  try {
+    await sql`
+      DELETE FROM invoices
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice'
+    }
+  }
+
+  revalidatePath('/dashboard/invoices');
+  // This isn't needed since we are already on the /dashboard/invoices url.
+  // redirect('/dashboard/invoices');
+}
